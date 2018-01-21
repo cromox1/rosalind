1c345ea5b73bdffe4ddb71df7cf524492067c4eb
Rosli Talib <xixa01@yahoo.co.uk>
2018-01-21 13:52:44 +0000

    Update test3DEG.py

--- test3DEG.py
+++ test3DEG.py
@@ -4,44 +4,42 @@ maksimum = 1 * 10**5
 
 def inputfile(filename):
     with open(filename) as f:
-        du = []
+        hubungan = {}
         mlist = [line.strip().split() for line in f.readlines()]
         f.close()
         print('mlist = ' + str(mlist))
         for inlist in mlist[1:]:
-            if int(inlist[0]) > int(inlist[1]):
-                inlist[0],inlist[1] = inlist[1],inlist[0]
-            # print('inlist ' + str(inlist))
-            du.append(int(inlist[0]))
-            du.append(int(inlist[1]))
-            du.sort()
+            if int(inlist[0]) not in hubungan:
+                hubungan[int(inlist[0])] = 1
+            else:
+                hubungan[int(inlist[0])] +=1
+            if int(inlist[1]) not in hubungan:
+                hubungan[int(inlist[1])] = 1
+            else:
+                hubungan[int(inlist[1])] +=1
 
-        m = len(du)
-        return du, m
+        m = len(hubungan)
+        return hubungan, m
 
 if __name__ == "__main__":
     filedirectory = r'C:\Users\cromox\Desktop\newselenium\rosalind\test4_degreearray'
-    # fileinput = r'\test_input1.txt'
-    fileinput = r'\rosalind_deg.txt'
+    fileinput = r'\test_input1.txt'
+    # fileinput = r'\rosalind_deg.txt'
     filename = filedirectory + fileinput
 
     baca = inputfile(filename)
     m = baca[1]
-    mlist = baca[0]
+    mnetx = baca[0]
 
-    try:
-        int(m)
+    if m <= maksimum and m == len(mnetx):
+        print('m = ' + str(m))
+        print('mnetx = ' + str(mnetx))
 
-        if m <= maksimum and m == len(mlist):
-            print('m = ' + str(m))
-            print('mlist = ' + str(mlist))
+        sortedmnetx = sorted(mnetx.items())
+        for k, v in sortedmnetx:
+            print(v, sep='', end=' ')
 
-            for val in list(set(mlist)):
-                print(mlist.count(val), sep=' ', end=' ')
-
-        else:
-            if m > maksimum:
-                terlebih = m
-            print('value exceeded %s' %maksimum + ' , ie %s' %terlebih)
-    except:
-        print('problem with input file %s' %fileinput)
+    else:
+        if m > maksimum:
+            terlebih = m
+        print('value exceeded %s' %maksimum + ' , ie %s' %terlebih)
