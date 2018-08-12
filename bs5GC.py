5c3f86fe71b572fe99bd69659032a1880ab08e39
Rosli Talib <xixa01@yahoo.co.uk>
2018-08-12 14:36:18 +0100

    Update bs5GC.py

--- bs5GC.py
+++ bs5GC.py
@@ -1,4 +1,18 @@
 __author__ = 'cromox'
+"""
+( http://rosalind.info/problems/gc/ )
+
+Problem
+
+The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. For example,
+the GC-content of "AGCTATAG" is 37.5%. Note that the reverse complement of any DNA string has the same GC-content.
+In Rosalind's implementation, a string in FASTA format will be labeled by the ID "Rosalind_xxxx", where "xxxx" denotes
+a four-digit code between 0000 and 9999.
+
+Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows
+for a default error of 0.001 in all decimal answers unless otherwise stated
+"""
 
 def inputfile(filename):
     with open(filename) as f:
