989802a0dac3addea6eb02349f3c999537127eae
Rosli Talib <xixa01@yahoo.co.uk>
2018-08-13 16:43:01 +0100

    Update q1_3LTR.py

--- q1_3LTR.py
+++ q1_3LTR.py
@@ -1,5 +1,3 @@
-__author__ = 'cromox'
-
 """
 Question 1:
 Write a function which takes as its input a string containing only the letters 'A', 'C', 'G', and 'T'.
@@ -15,25 +13,31 @@ TTA: 1
 The order of the sub-sequences in the output does not matter.
 """
 
+def check_actg(input):
+    for chr in input:
+        if chr not in 'ACGT':
+            return 0
+    return 1
+
 def count_3char(input1):
-    letterscan = []
-    for i in range(len(input1)-2):
-        char1 = input1[i:i+3]
-        if char1 in input1 and char1 not in letterscan:
-            letterscan.append(input1[i:i+3])
+    chkacgt = check_actg(input1)
+    if chkacgt == 1:
+        letterscan = []
+        for i in range(len(input1)-2):
+            if input1[i:i+3] in input1 and input1[i:i+3] not in letterscan:
+                letterscan.append(input1[i:i+3])
+        # print(letterscan)
 
-    print(letterscan)
+        count={}
+        for out1 in letterscan:
+            key,value = out1, input1.count(out1)
+            count[key] = value
+        # print(count)
 
-    count={}
-    for out1 in letterscan:
-        key,value = out1, input1.count(out1)
-        count[key] = value
-
-    print(count)
-
-    for key,value in count.items():
-        print(key+": "+str(value), end='\n')
-
-ss = "ACTACTTACGATAC"
-
+        for key,value in count.items():
+            print(key+": "+str(value), end='\n')
+    else:
+        print(input1 + ' problem - has char other than ACGT')
+        
+ss = "ACTACTTACG"
 count_3char(ss)
